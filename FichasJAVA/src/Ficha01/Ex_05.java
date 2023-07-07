f72fcac085e47e4cec51a750315ba7b4c8372033
Vitor Santos <vmvs007@gmail.com>
2023-07-07 11:29:02 +0100

    Ficha_01

--- FichasJAVA/src/Ficha01/Ex_05.java
+++ FichasJAVA/src/Ficha01/Ex_05.java
@@ -9,5 +9,28 @@ public class Ex_05 {
         Scanner input = new Scanner(System.in);
 
         System.out.println("***** Exercício 5 *****");
+
+        // Declarar variáveis
+        double nota1, nota2, nota3, media;
+
+        // Ler input do utilizador
+        System.out.print("Insira a Nota 1: ");
+        nota1 = input.nextDouble();
+
+        System.out.print("Insira a Nota 2: ");
+        nota2 = input.nextDouble();
+
+        System.out.print("Insira a Nota 3: ");
+        nota3 = input.nextDouble();
+
+        // Calcular media aritmetica
+        media= (nota1+nota2+nota3)/3;
+        System.out.println("Média Aritmética: " + media);
+
+        // Calcular a média ponderada
+        media = (nota1*0.2)+(nota2*0.3)+(nota3*0.5);
+        System.out.println("Media Ponderada: "+ media);
+
+
     }
 }
