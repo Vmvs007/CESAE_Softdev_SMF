5539f06fdb7467699705d8ee4f4e50f521298666
Vitor Santos <vmvs007@gmail.com>
2023-07-07 11:41:42 +0100

    Ficha_01

---
+++ FichasJAVA/src/Ficha01/Ex_06.java
@@ -0,0 +1,37 @@
+package Ficha01;
+
+import java.util.Scanner;
+
+public class Ex_06 {
+    public static void main(String[] args) {
+
+        // Instancia obrigatória do Scanner
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("***** Exercício 6 *****");
+
+        // Declarar variáveis
+        int valor1, valor2;
+
+        // Ler input do utilizador
+        System.out.print("Insira o valor1: ");
+        valor1=input.nextInt();
+
+        System.out.print("Insira o valor2: ");
+        valor2=input.nextInt();
+
+        // Apresentar variáveis iniciais
+        System.out.println("\nValor 1: "+valor1);
+        System.out.println("Valor 2: "+valor2);
+
+        // Trocar as variáveis
+         valor1=valor1+valor2;
+         valor2 = valor1 - valor2;
+         valor1=valor1-valor2;
+
+
+        // Apresentar as variáveis trocadas
+        System.out.println("\nValor 1 Trocado: "+valor1);
+        System.out.println("Valor 2 Trocado: "+valor2);
+    }
+}
