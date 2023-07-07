1d27a784922d09ce75f5a0818855e50379204cff
Vitor Santos <vmvs007@gmail.com>
2023-07-07 11:23:13 +0100

    Ficha_01

--- FichasJAVA/src/Ficha01/Ex_04.java
+++ FichasJAVA/src/Ficha01/Ex_04.java
@@ -12,6 +12,20 @@ public class Ex_04 {
         System.out.println("***** Exercício 4 *****");
 
         // Declarar variáveis
+        double raio,area;
+
+        // Declarar constantes
+        final double PI = 3.1415926;
+
+        // Ler input do utilizador
+        System.out.print("Insira o raio: ");
+        raio = input.nextDouble();
+
+        // Calcular a área da circuferência - pi*(raio*raio)
+        area= PI*(raio*raio);
+
+        // Apresentar a área
+        System.out.println("Área da circuferência: "+area);
     }
 
 
---
+++ FichasJAVA/src/Ficha01/Ex_05.java
@@ -0,0 +1,13 @@
+package Ficha01;
+
+import java.util.Scanner;
+
+public class Ex_05 {
+    public static void main(String[] args) {
+
+        // Instancia obrigatória do Scanner
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("***** Exercício 5 *****");
+    }
+}
