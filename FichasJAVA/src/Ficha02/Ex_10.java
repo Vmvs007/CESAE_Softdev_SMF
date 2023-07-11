a59ac0067b93f1aaf271cfe074fff94dad00fc76
Vitor Santos <vmvs007@gmail.com>
2023-07-11 15:25:48 +0100

    Ficha_02

--- FichasJAVA/src/Ficha02/Ex_10.java
+++ FichasJAVA/src/Ficha02/Ex_10.java
@@ -9,7 +9,45 @@ public class Ex_10 {
         Scanner input = new Scanner(System.in);
 
         // Declarar variáveis
-        
+        double num1,num2, resultado;
+        String operacao;
+
+        // Ler input do utilizador
+        System.out.print("Insira um numero: ");
+        num1 = input.nextDouble();
+
+        System.out.print("Insira outro numero: ");
+        num2 = input.nextDouble();
+
+        System.out.print("Escolha a operação (+ - * /): ");
+        operacao = input.next();
+
+        // Fazer a operação de acordo com o escolhido
+        switch (operacao){
+            case "+": // Soma
+                resultado= num1 + num2;
+                System.out.println("Soma: "+ resultado);
+                break;
+
+            case "-": // Subtração
+                resultado= num1 - num2;
+                System.out.println("Subtração: "+ resultado);
+                break;
+
+            case "*": // Multiplicação
+                resultado= num1 * num2;
+                System.out.println("Multiplicação: "+ resultado);
+                break;
+
+            case "/": // Divisão
+                resultado= num1 / num2;
+                System.out.println("Divisão: "+ resultado);
+                break;
+
+            default:
+                System.out.println("Operação Inválida!");
+                break;
+        }
 
     }
 }
