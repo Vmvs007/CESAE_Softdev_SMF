15f045987e520d2045c76633a1b32f31d57c471c
Vitor Santos <vmvs007@gmail.com>
2023-07-11 16:49:17 +0100

    Ficha02

--- FichasJAVA/src/Ficha02/Ex_12.java
+++ FichasJAVA/src/Ficha02/Ex_12.java
@@ -1,4 +1,47 @@
 package Ficha02;
 
+import java.util.Scanner;
+
 public class Ex_12 {
+    public static void main(String[] args) {
+
+        // Instancia obrigatória do Scanner
+        Scanner input = new Scanner(System.in);
+
+        // Declarar as variáveis
+        int opcao;
+
+        // Apresentar as opções
+        System.out.println("1 - Criar");
+        System.out.println("2 - Atualizar");
+        System.out.println("3 - Eliminar");
+        System.out.println("4 - Sair");
+
+        // Ler input do utilizador
+        System.out.print("\nEscolha uma opção: ");
+        opcao= input.nextInt();
+
+        // Imprimir a opção do utilizador
+        switch (opcao){
+            case 1:
+                System.out.println("Escolheu Criar");
+                break;
+
+            case 2:
+                System.out.println("Escolheu Atualizar");
+                break;
+
+            case 3:
+                System.out.println("Escolheu Eliminar");
+                break;
+
+            case 4:
+                break;
+
+            default:
+                System.out.println("Opção Inválida");
+                break;
+
+        }
+    }
 }
