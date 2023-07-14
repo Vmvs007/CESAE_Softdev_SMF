81a21e118f05e320b7ca6eb09622093422dada8e
Vitor Santos <vmvs007@gmail.com>
2023-07-14 15:24:02 +0100

    Ficha_03

--- FichasJAVA/src/Ficha03/Ex_09.java
+++ FichasJAVA/src/Ficha03/Ex_09.java
@@ -1,4 +1,22 @@
 package Ficha03;
 
+import java.util.Scanner;
+
 public class Ex_09 {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        // Declarar variáveis
+        int num, itr=2;
+
+        // Ler input do utilizador
+        System.out.print("Insira um número: ");
+        num = input.nextInt();
+
+        while (itr<= num){
+            System.out.println(itr);
+            itr=itr+2;
+        }
+    }
 }
--- FichasJAVA/src/Ficha03/Ex_10.java
+++ FichasJAVA/src/Ficha03/Ex_10.java
@@ -1,4 +1,26 @@
 package Ficha03;
 
+import java.util.Scanner;
+
 public class Ex_10 {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        // Declarar as variáveis
+        int limite, salto, numInicial=0;
+
+        // Ler input do utilizador
+        System.out.print("Insira o limite: ");
+        limite= input.nextInt();
+
+        System.out.print("Insira o salto: ");
+        salto= input.nextInt();
+
+        while(numInicial<=limite){
+            System.out.println(numInicial);
+            numInicial= numInicial+salto;
+        }
+
+    }
 }
