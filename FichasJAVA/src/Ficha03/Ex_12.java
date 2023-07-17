a27b6d27f08aa14b04bf366cf8bdc641bd3cedaa
Vitor Santos <vmvs007@gmail.com>
2023-07-17 08:28:44 +0100

    Ficha_03

--- FichasJAVA/src/Ficha03/Ex_12.java
+++ FichasJAVA/src/Ficha03/Ex_12.java
@@ -1,4 +1,26 @@
 package Ficha03;
 
+import java.util.Scanner;
 public class Ex_12 {
-}
+    public static void main(String[] args) {
+        // Import obrigatório do Scanner
+        Scanner input = new Scanner(System.in);
+
+        // Declarar variáveis
+        int inicio, limite;
+
+        // Ler input do Utilizador
+        System.out.print("Introduza o inicio do intervalo: ");
+        inicio = input.nextInt();
+
+        System.out.print("Introduza o fim do intervalo: ");
+        limite = input.nextInt();
+
+        while(inicio<=limite){
+            if(inicio%5==0){
+                System.out.println(inicio);
+            }
+            inicio++;
+        }
+
+    }}
