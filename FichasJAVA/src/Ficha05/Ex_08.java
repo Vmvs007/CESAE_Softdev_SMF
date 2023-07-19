b8a0e40dabab12dd80068d2e9126ef136b6626b1
Vitor Santos <vmvs007@gmail.com>
2023-07-19 14:45:20 +0100

    Ficha_05

--- FichasJAVA/src/Ficha05/Ex_08.java
+++ FichasJAVA/src/Ficha05/Ex_08.java
@@ -1,4 +1,30 @@
 package Ficha05;
 
+import java.util.Scanner;
+
 public class Ex_08 {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        // Declarar variáveis
+        int[][] matriz = new int[3][3];
+
+        // Ler input do utilizador
+        for(int linha=0; linha <3 ; linha++){
+            for(int coluna=0; coluna<3; coluna++){
+                System.out.print("Insira um número na matriz["+linha+"]["+coluna+"]: ");
+                matriz[linha][coluna]=input.nextInt();
+            }
+        }
+
+        for(int i=0; i <3 ; i++){
+            for(int j=0; j<3; j++){
+                System.out.print("Insira um número na matriz["+i+"]["+j+"]: ");
+                matriz[i][i]=input.nextInt();
+            }
+        }
+
+
+    }
 }
