0b9de90bc9393e9a06fcd10c072940fb6cfc0107
Vitor Santos <vmvs007@gmail.com>
2023-09-07 10:57:49 +0100

    Ficha_07

---
+++ FichasJAVA/src/Ficha07/Ex_01.java
@@ -0,0 +1,14 @@
+package Ficha07;
+
+public class Ex_01 {
+
+    public static void imprimirFicheiro(String path){
+
+    }
+
+    public static void main(String[] args) {
+
+        imprimirFicheiro("ficheiros/exercicio_01.txt");
+
+    }
+}
