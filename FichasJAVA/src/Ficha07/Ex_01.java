6acf0a5bd5f2a0b8c43e0b455cf6457707d5e7ca
Vitor Santos <vmvs007@gmail.com>
2023-09-07 11:07:15 +0100

    Ficha_07

--- FichasJAVA/src/Ficha07/Ex_01.java
+++ FichasJAVA/src/Ficha07/Ex_01.java
@@ -1,14 +1,26 @@
 package Ficha07;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Ex_01 {
 
-    public static void imprimirFicheiro(String path){
+    public static void imprimirFicheiro(String path) throws FileNotFoundException {
 
+        Scanner scannerFicheiro = new Scanner(new File(path));
+
+        while(scannerFicheiro.hasNextLine()){
+            System.out.println(scannerFicheiro.nextLine());
+        }
+
+        scannerFicheiro.close();
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
 
-        imprimirFicheiro("ficheiros/exercicio_01.txt");
+        imprimirFicheiro("ficheiros/exercicio_01_Alternativa02.txt");
+
 
     }
 }
